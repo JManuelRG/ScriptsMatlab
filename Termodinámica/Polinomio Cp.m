67c71172f2c38a5b54a8e73063748f717d735aaa
J Manuel RG <77511125+JManuelRG@users.noreply.github.com>
2021-11-16 16:48:28 -0600

    Create Polinomio Cp.m

---
+++ Termodinámica/Polinomio Cp.m
@@ -0,0 +1,27 @@
+%% Integración
+%% Integral del polinomio de Cp
+
+%Variables: Metano
+A = 1.702;
+B = 9.081e-3;
+C = -2.164e-6;
+D = 0;
+
+%%Valores de Temperatura
+T1=260+273.15;
+T2=600+273.15;
+
+%% Declaración de la función:
+syms T
+
+% Polinomio (función de T)
+CpR=A+B*T+C*T^2+D*T^-2;
+
+%% Obtener el valor de la integral
+integral=int(CpR,T,T1,T2);
+R=8.314;
+Q = R*integral;
+
+%% Salida de resultados
+fprintf("El valor calculado para el calor en J/mol es:")
+disp(double(Q))
